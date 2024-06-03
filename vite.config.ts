c8a44218ae57c7541dc72645d199ffdaf85384ff
Esteban Solis <estebansolisd@gmail.com>
2024-06-03 15:06:34 -0600

    adding micro front end integration

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import federation from "@originjs/vite-plugin-federation";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: 'todo_app',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './TodoList': './src/components/TodoList.tsx',
+        './TodoFilter': './src/components/TodoFilter.tsx',
+        './TodoCreation': './src/components/TodoList.tsx'
+      },
+      shared: ['react', 'react-dom'],
+    }),
+  ],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "/src") }],
   },
@@ -13,5 +27,5 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/__tests__/setup.ts"
-  }
+  },
 });
